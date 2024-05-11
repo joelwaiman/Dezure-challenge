29099edcd1475f332d7865db73f5d78c0feaea24
joelwaiman <waimanjoel@gmail.com>
2024-05-11 13:53:53 -0300

    more styles

--- src/app/Transcription/transcription.tsx
+++ src/app/Transcription/transcription.tsx
@@ -3,28 +3,30 @@ import { Message } from "../types/types"
 
 interface TranscriptionProps {
     transcription: Message[];
-  }
-  
+}
 
-const Transcription = ({transcription}: TranscriptionProps) => {
+
+const Transcription = ({ transcription }: TranscriptionProps) => {
 
     const matchTime = (time: number) => {
         const audioElement = document.getElementById('audio') as HTMLAudioElement;
-        if(audioElement){
+        if (audioElement) {
             audioElement.currentTime = time
         }
     }
 
-    return(
-        <section className="w-4/5 ">
-            <ul className="bg-gray-800 flex flex-wrap p-5 rounded-xl">
-                {transcription.map((message, index)=>{
+    return (
+        <section className="flex items-center w-4/5 h-screen">
+            <ul className="bg-[#333333] flex flex-wrap max-h-[75%] overflow-auto p-5 rounded-xl">
+                {transcription.map((message, index) => {
                     return <li key={index}
-                    className={message.role === 'user'? "flex flex-col w-full items-end p-3 rounded-xl" : "flex flex-col w-full p-3 rounded-xl"}
-                    onClick={()=>matchTime(message.start)}>
-                        <p className="font-bold m-2">{message.role}</p>
-                        <p className={message.role === 'user'? "max-w-[75%] bg-slate-950 p-3 rounded-xl cursor-pointer hover:scale-[1.025] transition duration-200 ease-in-out" : "max-w-[75%] bg-slate-500 p-3 rounded-xl cursor-pointer hover:scale-[1.025] transition duration-200 ease-in-out"}>{message.content}</p>
-                        </li>
+                        className={message.role === 'user' ? "flex flex-col w-full items-end p-3 rounded-xl" : "flex flex-col w-full p-3 rounded-xl"}
+                        onClick={() => matchTime(message.start)}>
+                        <p className="font-bold m-2 capitalize">
+                            {message.role}
+                        </p>
+                        <p className={message.role === 'user' ? "max-w-[75%] bg-[#364649] p-3 rounded-xl cursor-pointer hover:scale-[1.025] transition duration-200 ease-in-out" : "max-w-[75%] bg-[#708F96] p-3 rounded-xl cursor-pointer hover:scale-[1.025] transition duration-200 ease-in-out"}>{message.content}</p>
+                    </li>
                 })}
             </ul>
         </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ const textMessage: Message[] = [
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Transcription transcription={textMessage}/>
       <audio controls
       className="fixed bottom-5"

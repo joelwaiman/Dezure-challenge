e8b1e34fcf08e876a6504db6473bc68a29881f15
joelwaiman <waimanjoel@gmail.com>
2024-05-10 21:50:22 -0300

    styles

--- src/app/Transcription/transcription.tsx
+++ src/app/Transcription/transcription.tsx
@@ -16,14 +16,14 @@ const Transcription = ({transcription}: TranscriptionProps) => {
     }
 
     return(
-        <section className="w-3/5 ">
-            <ul className="bg-gray-800 flex flex-wrap p-5 ">
+        <section className="w-4/5 ">
+            <ul className="bg-gray-800 flex flex-wrap p-5 rounded-xl">
                 {transcription.map((message, index)=>{
                     return <li key={index}
                     className={message.role === 'user'? "flex flex-col w-full items-end p-3 rounded-xl" : "flex flex-col w-full p-3 rounded-xl"}
                     onClick={()=>matchTime(message.start)}>
-                        <p className="px-3 font-bold">{message.role}</p>
-                        <p className={message.role === 'user'? "max-w-[65%] bg-slate-950 p-3 rounded-xl cursor-pointer" : "max-w-[65%] bg-slate-500 p-3 rounded-xl cursor-pointer"}>{message.content}</p>
+                        <p className="font-bold m-2">{message.role}</p>
+                        <p className={message.role === 'user'? "max-w-[75%] bg-slate-950 p-3 rounded-xl cursor-pointer hover:scale-[1.025] transition duration-200 ease-in-out" : "max-w-[75%] bg-slate-500 p-3 rounded-xl cursor-pointer hover:scale-[1.025] transition duration-200 ease-in-out"}>{message.content}</p>
                         </li>
                 })}
             </ul>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ const textMessage: Message[] = [
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
       <Transcription transcription={textMessage}/>
       <audio controls
       className="fixed bottom-5"
